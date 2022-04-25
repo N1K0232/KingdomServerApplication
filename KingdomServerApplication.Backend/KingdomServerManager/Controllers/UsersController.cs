120686095854c29d57d3d845a710fa4ebb54b999
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-25 12:14:11 +0200

    added UsersController

---
+++ KingdomServerApplication.Backend/KingdomServerManager/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using KingdomServerManager.BusinessLayer.Services;
+using KingdomServerManager.Shared.Models;
+using KingdomServerManager.Shared.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KingdomServerManager.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class UsersController : ControllerBase
+{
+    private readonly IUsersService usersService;
+
+    public UsersController(IUsersService usersService)
+    {
+        this.usersService = usersService;
+    }
+
+    [HttpDelete("DeleteUser/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest("not valid id");
+        }
+
+        await usersService.DeleteAsync(id);
+        return Ok("user deleted");
+    }
+
+    [HttpDelete("DeleteUser/{userName}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(string userName)
+    {
+        if (string.IsNullOrEmpty(userName) || string.IsNullOrWhiteSpace(userName))
+        {
+            return BadRequest("not valid username");
+        }
+
+        await usersService.DeleteAsync(userName);
+        return Ok("user deleted");
+    }
+
+    [HttpGet("GetUser/{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(User))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetUser(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest("not valid id");
+        }
+
+        var user = await usersService.GetAsync(id);
+
+        return user != null ?
+            Ok(user) :
+            NotFound("user not found");
+    }
+
+    [HttpGet("GetUser/{userName}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(User))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetUser(string userName)
+    {
+        if (string.IsNullOrEmpty(userName) || string.IsNullOrWhiteSpace(userName))
+        {
+            return BadRequest("not valid username");
+        }
+
+        var user = await usersService.GetAsync(userName);
+
+        return user != null ?
+            Ok(user) :
+            NotFound("user not found");
+    }
+
+    [HttpPost("SaveUser")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(User))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> SaveUser([FromBody] SaveUserRequest request)
+    {
+        var user = await usersService.SaveAsync(request);
+        return Ok(user);
+    }
+}
--- KingdomServerApplication.Backend/KingdomServerManager/Program.cs
+++ KingdomServerApplication.Backend/KingdomServerManager/Program.cs
@@ -1,8 +1,21 @@
+using FluentValidation.AspNetCore;
+using Hellang.Middleware.ProblemDetails;
+using KingdomServerManager.BusinessLayer.MapperProfiles;
+using KingdomServerManager.BusinessLayer.Services;
+using KingdomServerManager.BusinessLayer.Validators;
+using KingdomServerManager.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using TinyHelpers.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+services.AddProblemDetails();
+services.AddAutoMapper(typeof(UserMapperProfile).Assembly);
+services.AddFluentValidation(options =>
+{
+    options.RegisterValidatorsFromAssemblyContaining<SaveUserRequestValidator>();
+});
 services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
@@ -11,11 +24,23 @@ services.AddControllers().AddJsonOptions(options =>
 });
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+services.AddDbContext<DataContext>(options =>
+{
+    var configuration = builder.Configuration;
+    var connectionString = configuration.GetConnectionString("SqlConnection");
+    options.UseSqlServer(connectionString);
+});
+services.AddScoped<IDataContext>(sp => sp.GetRequiredService<DataContext>());
+services.AddScoped<IReadOnlyDataContext>(sp => sp.GetRequiredService<DataContext>());
+services.AddScoped<IRolesService, RolesService>();
+services.AddScoped<IUsersService, UsersService>();
+services.AddScoped<IServerLogsService, ServerLogsService>();
 
 var app = builder.Build();
+app.UseProblemDetails();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.Run();
+await app.RunAsync();
